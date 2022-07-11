6be983c4feb91124102a3c0673de60dc6f138ef5
Alexey Avelchuk <aals@ya.ru>
2022-07-11 17:36:21 +0300

    button replacement password

--- target/scala-2.13/twirl/main/views/html/login.template.scala
+++ target/scala-2.13/twirl/main/views/html/login.template.scala
@@ -20,33 +20,24 @@ import play.api.data.Field
 import play.data._
 import play.core.j.PlayFormsMagicForJava._
 import scala.jdk.CollectionConverters._
-/*15.2*/import play.mvc.Http.Request
+/*1.2*/import play.mvc.Http.Request
 
 object login extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template3[Form[PersonData],Request,play.i18n.Messages,play.twirl.api.HtmlFormat.Appendable] {
 
   /**/
-  def apply/*18.2*/( form: Form[PersonData])(implicit request: Request, messages: play.i18n.Messages):play.twirl.api.HtmlFormat.Appendable = {
+  def apply/*3.2*/(form: Form[PersonData])(implicit request: Request, messages: play.i18n.Messages):play.twirl.api.HtmlFormat.Appendable = {
     _display_ {
       {
 
 
-Seq[Any](format.raw/*19.1*/("""
-"""),_display_(/*20.2*/helper/*20.8*/.form(routes.PersonController.checkPerson())/*20.52*/ {_display_(Seq[Any](format.raw/*20.54*/("""
-    """),_display_(/*21.6*/helper/*21.12*/.CSRF.formField),format.raw/*21.27*/("""
-       """),_display_(/*22.9*/helper/*22.15*/.inputText(form("email"))),format.raw/*22.40*/("""
-        """),_display_(/*23.10*/helper/*23.16*/.inputText(form("password"))),format.raw/*23.44*/("""
-
-   """),format.raw/*25.4*/("""<button type="submit">Login</button >
-
-""")))}),format.raw/*27.2*/("""
-
-      """),_display_(/*29.8*/helper/*29.14*/.form(routes.PersonController.registrationPerson())/*29.65*/ {_display_(Seq[Any](format.raw/*29.67*/("""
-            """),_display_(/*30.14*/helper/*30.20*/.CSRF.formField),format.raw/*30.35*/("""
-   """),_display_(/*31.5*/helper/*31.11*/.inputText(form("email"))),format.raw/*31.36*/("""
-    """),_display_(/*32.6*/helper/*32.12*/.inputText(form("password"))),format.raw/*32.40*/("""
-        """),format.raw/*33.9*/("""<button type="submit">Registration</button>
-""")))}),format.raw/*34.2*/("""
-
+Seq[Any](format.raw/*4.1*/("""
+"""),format.raw/*5.1*/("""<form name="PersonData" method="post">
+    """),_display_(/*6.6*/helper/*6.12*/.CSRF.formField),format.raw/*6.27*/("""  """),format.raw/*6.29*/("""<!-- This auto-generates a token for you -->
+    """),_display_(/*7.6*/helper/*7.12*/.inputText(form("email"))),format.raw/*7.37*/("""
+    """),_display_(/*8.6*/helper/*8.12*/.inputPassword(form("password"))),format.raw/*8.44*/("""
+    """),format.raw/*9.5*/("""<button autofocus type="submit" formaction="/login" >Вход</button>
+    <button type="submit" formaction="/registration">Регистрация</button>
+    </form>
 """))
       }
     }
@@ -64,9 +55,9 @@ Seq[Any](format.raw/*19.1*/("""
               /*
                   -- GENERATED --
                   SOURCE: app/views/login.scala.html
-                  HASH: c8fdcb53b9e59549d31bcfe63945184a5e91378e
-                  MATRIX: 611->564|982->596|1159->679|1187->681|1201->687|1254->731|1294->733|1326->739|1341->745|1377->760|1412->769|1427->775|1473->800|1510->810|1525->816|1574->844|1606->849|1676->889|1711->898|1726->904|1786->955|1826->957|1867->971|1882->977|1918->992|1949->997|1964->1003|2010->1028|2042->1034|2057->1040|2106->1068|2142->1077|2217->1122
-                  LINES: 23->15|28->18|33->19|34->20|34->20|34->20|34->20|35->21|35->21|35->21|36->22|36->22|36->22|37->23|37->23|37->23|39->25|41->27|43->29|43->29|43->29|43->29|44->30|44->30|44->30|45->31|45->31|45->31|46->32|46->32|46->32|47->33|48->34
+                  HASH: 09f536dba3b4e2c27880f3afbf21921480a0d9ab
+                  MATRIX: 610->1|980->32|1155->114|1182->115|1251->159|1265->165|1300->180|1329->182|1404->232|1418->238|1463->263|1494->269|1508->275|1560->307|1591->312
+                  LINES: 23->1|28->3|33->4|34->5|35->6|35->6|35->6|35->6|36->7|36->7|36->7|37->8|37->8|37->8|38->9
                   -- GENERATED --
               */
           
